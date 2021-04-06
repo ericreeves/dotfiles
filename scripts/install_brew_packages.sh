ece5493e03fe45158c0087de4efb3a87b69d2be5
ericreeves <eric@alluvium.com>
2021-04-05 23:20:18 -0500

    Make it work in Linux or OSX

---
+++ scripts/install_brew_packages.sh
@@ -0,0 +1,49 @@
+#!/usr/bin/env bash
+echo "Updating Brew"
+brew update
+
+TAPS="aws/tap wallix/awless homebrew/cask-fonts"
+for t in ${TAPS}; do
+    echo "Tapping ${t}..."
+    brew tap ${t}
+done
+
+PACKAGES="git hub tmux bat fzf ctags \
+readline awscli aws-iam-authenticator aws-sam-cli \
+awless cfn-lint helm htop iftop jq kops lame nmap \
+python sops terraform watch wget zsh ack coreutils fluxctl \
+eksctl aws-es-proxy yaml2json vcpkg"
+
+for p in ${PACKAGES}; do 
+    echo "Installing ${p}..."
+    brew install ${p}
+done
+
+echo "Cleaning up..."
+brew cleanup
+
+
+if [ "$(uname)" == "Darwin" ]; then
+
+echo "Installing cask..."
+CASKS="hyper slack visual-studio-code steam 1password \
+macdown aws-vault bartender boom-3d caffeine disk-inventory-x \
+docker dropbox google-chrome google-drive-file-stream \
+jira-client karabiner-elements macpass spectacle stellarium \
+steermouse the-unarchiver zoomus font-hack-nerd-font discord \
+vlc"
+
+for c in ${CASKS}; do
+    echo "Installing ${c}..."
+    brew cask install ${c}
+done
+
+fi
+
+#echo "Installing Python packages..."
+#PYTHON_PACKAGES=(
+#    virtualenv
+#    virtualenvwrapper
+#)
+#sudo pip install ${PYTHON_PACKAGES[@]}
+
