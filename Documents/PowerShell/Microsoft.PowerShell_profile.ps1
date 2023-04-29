2f3b8a8ffdcb310a6e59aa87050d5a058970522c
ericreeves <eric@alluvium.com>
2023-04-29 13:24:17 -0500

    Update Documents/PowerShell/Microsoft.PowerShell_profile.ps1

--- Documents/PowerShell/Microsoft.PowerShell_profile.ps1
+++ Documents/PowerShell/Microsoft.PowerShell_profile.ps1
@@ -7,29 +7,29 @@ function rehash {
 if ($host.Name -eq 'ConsoleHost')
 {
   Import-Module PSReadLine
-  Import-Module posh-git
   Import-Module -Name Terminal-Icons
 
-  oh-my-posh --init --shell pwsh --config "$env:POSH_THEMES_PATH\M365Princess.omp.json" | Invoke-Expression
+  Invoke-Expression (&starship init powershell)
+
+# PSReadLine
+  Set-PSReadlineOption -EditMode vi
   Set-PSReadlineKeyHandler -Key Tab -Function Complete
 
   #Style
-  set-psreadlineoption -PredictionViewStyle ListView
-  set-psreadlineoption -PredictionViewStyle InlineView
+  Set-PSReadlineOption -PredictionViewStyle InlineView
 
 # Autocompletion for arrow keys
   Set-PSReadlineKeyHandler -Key UpArrow -Function HistorySearchBackward
   Set-PSReadlineKeyHandler -Key DownArrow -Function HistorySearchForward
 # auto suggestions
   Set-PSReadLineOption -PredictionSource History
-  Set-PSReadLineOption -EditMode Windows
-  Set-PSReadLineOption -PredictionViewStyle ListView
-  Set-PSReadlineOption -EditMode vi
 }
 
 # dir w/ fzf
 # fzf --preview 'bat --color=always --style=numbers --line-range=:500 {}'
 
-#Invoke-Expression (&starship init powershell)
 
 $env:Path = [System.Environment]::ExpandEnvironmentVariables([System.Environment]::GetEnvironmentVariable("Path","Machine") + ";" + [System.Environment]::GetEnvironmentVariable("Path","User"))
+
+Set-Alias -Name lvim -Value "$HOME\.local\bin\lvim.ps1"
+
