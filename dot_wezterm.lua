fd43ef3fce31656cce8cec0127bb505446d2080e
Eric Reeves <eric@alluvium.com>
2023-04-28 23:22:58 -0500

    Update .wezterm.lua

--- dot_wezterm.lua
+++ dot_wezterm.lua
@@ -1,4 +1,5 @@
 local wt = require("wezterm");
+local act = wt.action
 
 local TabBackground = "#000"
 local TabForeground = "#aaa"
@@ -27,7 +28,12 @@ wt.on("format-window-title", function(tab, pane, tabs, panes, config)
 end)
 
 return {
-	color_scheme = "Builtin Tango Dark",
+--	color_scheme = "Builtin Tango Dark",
+--  color_scheme = 'Tangoesque (terminal.sexy)',
+  color_scheme = 'Tomorrow Night Bright',
+
+--  color_scheme = 'Tango (terminal.sexy)',
+
 	colors = {
 		tab_bar = {
 			background = TabBackground,
@@ -79,35 +85,22 @@ return {
 		bottom = 0,
 	},
 	keys = {
-		{
-			key = "t",
-			mods = "CTRL",
-			action = wt.action{
-				SpawnTab = "DefaultDomain",
-			},
-		},
-		{
-			key = "w",
-			mods = "CTRL",
-			action = wt.action{
-				CloseCurrentTab = {
-					confirm = false,
-				},
-			},
-		},
-		{
-			key = "Tab",
-			mods = "CTRL",
-			action = wt.action{
-				ActivateTabRelative = 1,
-			},
-		},
-		{
-			key = "Tab",
-			mods = "CTRL|SHIFT",
-			action = wt.action{
-				ActivateTabRelative = -1,
-			},
-		},
-	},
+		{ key = "t", mods = "CTRL|SHIFT", action = wt.action{ SpawnTab = "DefaultDomain", }, },
+		{ key = "w", mods = "CTRL|SHIFT", action = wt.action{ CloseCurrentTab = { confirm = false, }, }, },
+		{ key = "Tab", mods = "CTRL", action = wt.action{ ActivateTabRelative = 1, }, },
+		{ key = "Tab", mods = "CTRL|SHIFT", action = wt.action{ ActivateTabRelative = -1, }, },
+		{ key = "c", mods = "ALT", action = wt.action{ CopyTo = "Clipboard", }, },
+		{ key = "v", mods = "ALT", action = wt.action{ PasteFrom = "Clipboard", }, },
+    { key = '>', mods = 'SHIFT|CTRL', action = wt.action.SplitVertical{ domain = 'CurrentPaneDomain' } },
+    { key = '<', mods = 'SHIFT|CTRL', action = wt.action.SplitHorizontal{ domain = 'CurrentPaneDomain' } },
+    { key = 'z', mods = 'SHIFT|CTRL', action = wt.action.TogglePaneZoomState },
+    { key = 'h', mods = 'SHIFT|CTRL', action = wt.action.ActivatePaneDirection 'Left' },
+    { key = 'l', mods = 'SHIFT|CTRL', action = wt.action.ActivatePaneDirection 'Right' },
+    { key = 'k', mods = 'SHIFT|CTRL', action = wt.action.ActivatePaneDirection 'Up' },
+    { key = 'j', mods = 'SHIFT|CTRL', action = wt.action.ActivatePaneDirection 'Down' },
+    { key = 'u', mods = 'SHIFT|CTRL', action = wt.action.AdjustPaneSize{ 'Left', 5 } },
+    { key = 'p', mods = 'SHIFT|CTRL', action = wt.action.AdjustPaneSize{ 'Right', 5 } },
+    { key = 'o', mods = 'SHIFT|CTRL', action = wt.action.AdjustPaneSize{ 'Up', 5 } },
+    { key = 'i', mods = 'SHIFT|CTRL', action = wt.action.AdjustPaneSize{ 'Down', 5 } }
+	}
 }
