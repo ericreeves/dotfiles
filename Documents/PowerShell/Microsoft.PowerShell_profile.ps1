eac5d0ce986de234611159f2755e8a4def85275b
Eric Reeves <eric@alluvium.com>
2023-04-30 19:16:43 -0500

    Update Documents/PowerShell/Microsoft.PowerShell_profile.ps1

--- Documents/PowerShell/Microsoft.PowerShell_profile.ps1
+++ Documents/PowerShell/Microsoft.PowerShell_profile.ps1
@@ -1,8 +1,13 @@
-function rehash {
-    $env:Path = [System.Environment]::GetEnvironmentVariable("Path","Machine") +
-                ";" +
-                [System.Environment]::GetEnvironmentVariable("Path","User")
-}
+# Environment 
+$Env:XDG_CONFIG_HOME = "$( $HOME )\.config"
+$Env:PATH = [System.Environment]::ExpandEnvironmentVariables([System.Environment]::GetEnvironmentVariable("Path","Machine") + ";" + [System.Environment]::GetEnvironmentVariable("Path","User"))
+
+# Aliases
+Set-Alias -Name lvim -Value "$( $HOME )\.local\bin\lvim.ps1"
+Set-Alias -Name vim -Value "$( $HOME )\.local\bin\lvim.ps1"
+Set-Alias -Name l -Value "lsd -l"
+Set-Alias -Name cm -Value "chezmoi"
+
 
 # Prompt
 if (Get-Command "starship" -ErrorAction SilentlyContinue) {
@@ -20,59 +25,44 @@ if ($host.Name -eq 'ConsoleHost')
 # PSReadLine
   Set-PSReadlineOption -EditMode vi
   Set-PSReadlineKeyHandler -Key Tab -Function Complete
-
-  #Style
   #Set-PSReadlineOption -PredictionViewStyle InlineView
   Set-PSReadlineOption -PredictionViewStyle ListView
-
-# Autocompletion for arrow keys
   Set-PSReadlineKeyHandler -Key UpArrow -Function HistorySearchBackward
   Set-PSReadlineKeyHandler -Key DownArrow -Function HistorySearchForward
-# auto suggestions
+  Set-PSReadlineKeyHandler -Key k -Function HistorySearchBackward
+  Set-PSReadlineKeyHandler -Key j -Function HistorySearchForward
   Set-PSReadLineOption -PredictionSource History
-}
-
-# Prompt
-if (Get-Command "starship" -ErrorAction SilentlyContinue) {
-  $ENV:STARSHIP_CONFIG = "$( $HOME )/.config/starship.toml"
-  # $ENV:STARSHIP_DISTRO = "SKY"
-  Invoke-Expression (&starship init powershell)
 }
 
 # dir w/ fzf
 # fzf --preview 'bat --color=always --style=numbers --line-range=:500 {}'
 
-$Env:KOMOREBI_CONFIG_HOME = 'C:\Users\LGUG2Z\.config\komorebi'
-
-$Env:PATH = [System.Environment]::ExpandEnvironmentVariables([System.Environment]::GetEnvironmentVariable("Path","Machine") + ";" + [System.Environment]::GetEnvironmentVariable("Path","User"))
-
-Set-Alias -Name lvim -Value "$HOME\.local\bin\lvim.ps1"
-Set-Alias -Name vim -Value "$HOME\.local\bin\lvim.ps1"
-Set-Alias -Name l -Value "lsd -l"
-
+# This function rehashes the environment variable Path by combining the Machine and User paths. 
+function rehash {
+  $env:Path = [System.Environment]::GetEnvironmentVariable("Path","Machine") +
+              ";" +
+              [System.Environment]::GetEnvironmentVariable("Path","User")
+}
 
 function Get-Process-Command {
   param (
-      [Parameter(Mandatory=$true)]
-      [string]$Name
+    [Parameter(Mandatory=$true)]
+    [string]$Name
   )
   Get-WmiObject Win32_Process -Filter "name = '$Name.exe'" -ErrorAction SilentlyContinue | Select-Object CommandLine,ProcessId
 }
 
 function Wait-For-Process {
   param (
-      [Parameter(Mandatory=$true)]
-      [string]$Name,
-
-      [Switch]$IgnoreExistingProcesses
+    [Parameter(Mandatory=$true)]
+    [string]$Name,
+    [Switch]$IgnoreExistingProcesses
   )
-
   if ($IgnoreExistingProcesses) {
       $NumberOfProcesses = (Get-Process -Name $Name -ErrorAction SilentlyContinue).Count
   } else {
       $NumberOfProcesses = 0
   }
-
   while ( (Get-Process -Name $Name -ErrorAction SilentlyContinue).Count -eq $NumberOfProcesses ) {
       Start-Sleep -Milliseconds 400
   }
@@ -81,47 +71,46 @@ function Wait-For-Process {
 ## Which
 function which {
   param(
-      [Parameter(Mandatory=$true)]
-      [string]$Command
+    [Parameter(Mandatory=$true)]
+    [string]$Command
   )
-
   Get-Command -Name $Command -ErrorAction SilentlyContinue |
   Select-Object -ExpandProperty Path -ErrorAction SilentlyContinue
 }
 
-
+# Komorebi
 if (Get-Command "komorebic" -ErrorAction SilentlyContinue) {
-  $ENV:KOMOREBI_CONFIG_HOME = "$( $HOME )/.config/komorebi"
-  $ENV:KOMOREBI_AHK_EXE = "$( $HOME )/scoop/apps/autohotkey/current/AutoHotkey64.exe"
+    $ENV:KOMOREBI_CONFIG_HOME = "$( $HOME )/.config/komorebi"
+    $ENV:KOMOREBI_AHK_EXE = "$( $HOME )/scoop/apps/autohotkey/current/v2/AutoHotkey64.exe"
 
-  function start-tiling {
-      Write-Host "[komorebi] Killing prior komorebi.exe Processes"
-      Get-Process -Name "komorebi" -ErrorAction SilentlyContinue | Stop-Process -Force -ErrorAction SilentlyContinue
+    function start-tiling {
+        Write-Host "[komorebi] Killing prior komorebi.exe Processes"
+        Get-Process -Name "komorebi" -ErrorAction SilentlyContinue | Stop-Process -Force -ErrorAction SilentlyContinue
 
-      Write-Host "[komorebi] Running komorebic.exe start"
-      Execute-Command -FilePath "$( $HOME )/scoop/apps/komorebi/current/komorebic.exe" -ArgumentList "start" -WorkingDirectory "$( $HOME )/.config/komorebi" -ErrorAction SilentlyContinue
-      Wait-For-Process -Name "komorebi"
-      Start-Sleep 3
+        Write-Host "[komorebi] Running komorebic.exe start"
+        Execute-Command -FilePath "$( $HOME )/scoop/apps/komorebi/current/komorebic.exe" -ArgumentList "start" -WorkingDirectory "$( $HOME )/.config/komorebi" -ErrorAction SilentlyContinue
+        Wait-For-Process -Name "komorebi"
+        Start-Sleep 3
 
-      Write-Host "[ahk] Starting AutoHotKey"
-      Execute-Command -FilePath "$( $HOME )/scoop/apps/autohotkey/current/AutoHotkey64.exe" -ArgumentList "$( $HOME )/.config/komorebi/komorebi.ahk" -WorkingDirectory "$( $HOME )/.config/komorebi" -ErrorAction SilentlyContinue
-  }
+        Write-Host "[ahk] Starting AutoHotKey"
+        Execute-Command -FilePath "$( $HOME )/scoop/apps/autohotkey/current/AutoHotkey64.exe" -ArgumentList "$( $HOME )/.config/komorebi/komorebi.ahk" -WorkingDirectory "$( $HOME )/.config/komorebi" -ErrorAction SilentlyContinue
+    }
 
-  function stop-tiling {
-      Write-Host "[komorebi] Issuing komorebic stop"
+    function stop-tiling {
+        Write-Host "[komorebi] Issuing komorebic stop"
 
-      Execute-Command -FilePath "$( $HOME )/scoop/apps/komorebi/current/komorebic.exe" -ArgumentList "stop" -WorkingDirectory "$( $HOME )/.config/komorebi" -ErrorAction SilentlyContinue
-      Write-Host "[komorebi] Terminating Remaining komorebi Processes"
-      Get-Process -Name "komorebi" -ErrorAction SilentlyContinue | Stop-Process -Force -ErrorAction SilentlyContinue
-      Wait-Process -Name "komorebi" -ErrorAction SilentlyContinue
+        Execute-Command -FilePath "$( $HOME )/scoop/apps/komorebi/current/komorebic.exe" -ArgumentList "stop" -WorkingDirectory "$( $HOME )/.config/komorebi" -ErrorAction SilentlyContinue
+        Write-Host "[komorebi] Terminating Remaining komorebi Processes"
+        Get-Process -Name "komorebi" -ErrorAction SilentlyContinue | Stop-Process -Force -ErrorAction SilentlyContinue
+        Wait-Process -Name "komorebi" -ErrorAction SilentlyContinue
 
-      Write-Host "[komorebi] Checking for Processes"
-      Get-Process-Command -Name "komorebi" -ErrorAction SilentlyContinue
+        Write-Host "[komorebi] Checking for Processes"
+        Get-Process-Command -Name "komorebi" -ErrorAction SilentlyContinue
 
-      Write-Host "[ahk] Killing Process"
-      Get-Process -Name "AutoHotKey" -ErrorAction SilentlyContinue | Stop-Process -Force -ErrorAction SilentlyContinue
-      Wait-Process -Name "AutoHotKey" -ErrorAction SilentlyContinue
-  }
+        Write-Host "[ahk] Killing Process"
+        Get-Process -Name "AutoHotKey" -ErrorAction SilentlyContinue | Stop-Process -Force -ErrorAction SilentlyContinue
+        Wait-Process -Name "AutoHotKey" -ErrorAction SilentlyContinue
+    }
 
 }
 
