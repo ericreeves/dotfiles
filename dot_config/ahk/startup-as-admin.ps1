5d79ab1bb86a58ebcae86c9b7705f85d1a52c1fc
Eric Reeves <eric@alluvium.com>
2023-10-22 17:46:58 -0500

    Update .config/ahk/startup-as-admin.ps1

--- dot_config/ahk/startup-as-admin.ps1
+++ dot_config/ahk/startup-as-admin.ps1
@@ -1,11 +1,12 @@
 # Run the following commands in an Administrator powershell prompt. 
 # Be sure to specify the correct path to your desktop_switcher.ahk file. 
 
-Unregister-ScheduledTask DesktopSwitcher 
-$A = New-ScheduledTaskAction -Execute "$PSScriptRoot\desktop_switcher.ahk"
+Unregister-ScheduledTask DesktopSwitcher -Confirm:$false
+$A = New-ScheduledTaskAction -Execute "$HOME\scoop\apps\nircmd\current\nircmd.exe" -Argument "execmd $HOME\scoop\shims\autohotkey.exe $HOME\.config\ahk\init.ahk"
 $T = New-ScheduledTaskTrigger -AtLogon
 $P = New-ScheduledTaskPrincipal -GroupId "BUILTIN\Administrators" -RunLevel Highest
-$S = New-ScheduledTaskSettingsSet -AllowStartIfOnBatteries -DontStopIfGoingOnBatteries -ExecutionTimeLimit 0
+# $S = New-ScheduledTaskSettingsSet -RestartCount:100000 -RestartInterval (New-TimeSpan -Minutes 1) -AllowStartIfOnBatteries -DontStopIfGoingOnBatteries -ExecutionTimeLimit 0 -Compatibility Win8 -MultipleInstances IgnoreNew -Hidden -StartWhenAvailable
+$S = New-ScheduledTaskSettingsSet -AllowStartIfOnBatteries -DontStopIfGoingOnBatteries -ExecutionTimeLimit 0 -Compatibility Win8 -MultipleInstances IgnoreNew -Hidden -StartWhenAvailable
 $D = New-ScheduledTask -Action $A -Principal $P -Trigger $T -Settings $S
 Register-ScheduledTask DesktopSwitcher -InputObject $D
 Start-ScheduledTask DesktopSwitcher 
