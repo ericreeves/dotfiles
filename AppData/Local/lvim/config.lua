99255dfdf05a2f929d939249df5cc8e20c1d8059
Eric Reeves <eric@alluvium.com>
2024-12-01 11:41:33 -0600

    Update AppData/Local/lvim/config.lua

--- AppData/Local/lvim/config.lua
+++ AppData/Local/lvim/config.lua
@@ -1,280 +1,44 @@
 -- Read the docs: https://www.lunarvim.org/docs/configuration
--- Forum: https://www.reddit.com/r/lunarvim/
+-- Example configs: https://github.com/LunarVim/starter.lvim
 -- Video Tutorials: https://www.youtube.com/watch?v=sFA9kX-Ud_c&list=PLhoH5vyxr6QqGu0i7tt_XoVK9v-KvZ3m6
+-- Forum: https://www.reddit.com/r/lunarvim/
 -- Discord: https://discord.com/invite/Xb9B4Ny
---
 
---[[
- THESE ARE EXAMPLE CONFIGS FEEL FREE TO CHANGE TO WHATEVER YOU WANT
- `lvim` is the global options object
-]]
 -- Enable powershell as your default shell
-
--- #############################################
--- Windows Crap
--- #############################################
--- vim.opt.shell = "pwsh.exe -NoLogo"
--- vim.opt.shellcmdflag =
--- "-NoLogo -NoProfile -ExecutionPolicy RemoteSigned -Command [Console]::InputEncoding=[Console]::OutputEncoding=[System.Text.Encoding]::UTF8;"
--- vim.cmd [[
--- 		let &shellredir = '2>&1 | Out-File -Encoding UTF8 %s; exit $LastExitCode'
--- 		let &shellpipe = '2>&1 | Out-File -Encoding UTF8 %s; exit $LastExitCode'
--- 		set shellquote= shellxquote=
---   ]]
+vim.opt.shell = "pwsh.exe"
+vim.opt.shellcmdflag =
+  "-NoLogo -NoProfile -ExecutionPolicy RemoteSigned -Command [Console]::InputEncoding=[Console]::OutputEncoding=[System.Text.Encoding]::UTF8;"
+vim.cmd [[
+		let &shellredir = '2>&1 | Out-File -Encoding UTF8 %s; exit $LastExitCode'
+		let &shellpipe = '2>&1 | Out-File -Encoding UTF8 %s; exit $LastExitCode'
+		set shellquote= shellxquote=
+  ]]
 
 -- Set a compatible clipboard manager
--- vim.g.clipboard = {
---   copy = {
---     ["+"] = "win32yank.exe -i --crlf",
---     ["*"] = "win32yank.exe -i --crlf",
---   },
---   paste = {
---     ["+"] = "win32yank.exe -o --lf",
---     ["*"] = "win32yank.exe -o --lf",
---   },
--- }
--- #############################################
--- End Windows Crap
--- #############################################
-
--- vim options
--- vim.opt.shiftwidth = 2
--- vim.opt.tabstop = 2
--- vim.opt.relativenumber = true
-
--- general
-lvim.log.level = "info"
-lvim.format_on_save = {
-	enabled = true,
-	pattern = "*.lua",
-	timeout = 1000,
+vim.g.clipboard = {
+  copy = {
+    ["+"] = "win32yank.exe -i --crlf",
+    ["*"] = "win32yank.exe -i --crlf",
+  },
+  paste = {
+    ["+"] = "win32yank.exe -o --lf",
+    ["*"] = "win32yank.exe -o --lf",
+  },
 }
 
--- keymappings <https://www.lunarvim.org/docs/configuration/keybindings>
-lvim.leader = "space"
--- add your own keymapping
-lvim.keys.normal_mode["<C-s>"] = ":w<cr>"
-lvim.keys.normal_mode["<C-q>"] = ":q<cr>"
-
-lvim.keys.normal_mode["<S-l>"] = ":BufferLineCycleNext<CR>"
-lvim.keys.normal_mode["<S-h>"] = ":BufferLineCyclePrev<CR>"
-
--- -- Use which-key to add extra bindings with the leader-key prefix
--- lvim.builtin.which_key.mappings["W"] = { "<cmd>noautocmd w<cr>", "Save without formatting" }
--- lvim.builtin.which_key.mappings["P"] = { "<cmd>Telescope projects<CR>", "Projects" }
-lvim.builtin.which_key.mappings["e"] = { "<cmd>Neotree toggle<cr>", "Toggle Neotree" }
-
-lvim.builtin.which_key.mappings["\\"] = { "<cmd>Neotree reveal<cr>", "Reveal Neotree" }
-
-lvim.builtin.telescope.theme = "dropdown"
-lvim.builtin.telescope.defaults.initial_mode = "insert"
-lvim.builtin.telescope.defaults.layout_config.prompt_position = "bottom"
--- lvim.builtin.telescope.defaults.layout_config.preview_cutoff = 120
--- lvim.builtin.telescope.defaults.layout_config.vertical.mirror = false
--- lvim.builtin.telescope.defaults.layout_config.horizontal.mirror = false
--- lvim.builtin.telescope.defaults.layout_strategy = "horizontal"
-lvim.builtin.telescope.defaults.layout_config.width = 0.95
-
--- -- Change theme settings
--- lvim.colorscheme = "lunaperche"
--- lvim.colorscheme = "lunar"
--- lvim.colorscheme = "catppuccin"
-lvim.colorscheme = "catppuccin-mocha"
-lvim.transparent_window = true
-
-lvim.builtin.alpha.active = true
-lvim.builtin.alpha.mode = "dashboard"
-lvim.builtin.terminal.active = true
-
--- Automatically install missing parsers when entering buffer
-lvim.builtin.treesitter.auto_install = true
-
--- lvim.builtin.treesitter.ignore_install = { "haskell" }
-
--- -- ensure these parsers are always installed, useful for those without a strict filetype
-lvim.builtin.treesitter.ensure_installed = { "comment", "markdown_inline", "regex" }
-
--- -- generic LSP settings <https://www.lunarvim.org/docs/languages#lsp-support>
-
--- ---configure a server manually. IMPORTANT: Requires `:LvimCacheReset` to take effect
--- ---see the full default list `:lua =lvim.lsp.automatic_configuration.skipped_servers`
--- vim.list_extend(lvim.lsp.automatic_configuration.skipped_servers, { "pyright" })
--- local opts = {} -- check the lspconfig documentation for a list of all possible options
--- require("lvim.lsp.manager").setup("pyright", opts)
-
--- ---remove a server from the skipped list, e.g. eslint, or emmet_ls. IMPORTANT: Requires `:LvimCacheReset` to take effect
--- ---`:LvimInfo` lists which server(s) are skipped for the current filetype
--- lvim.lsp.automatic_configuration.skipped_servers = vim.tbl_filter(function(server)
---   return server ~= "emmet_ls"
--- end, lvim.lsp.automatic_configuration.skipped_servers)
-
--- -- you can set a custom on_attach function that will be used for all the language servers
--- -- See <https://github.com/neovim/nvim-lspconfig#keybindings-and-completion>
--- lvim.lsp.on_attach_callback = function(client, bufnr)
---   local function buf_set_option(...)
---     vim.api.nvim_buf_set_option(bufnr, ...)
---   end
---   --Enable completion triggered by <c-x><c-o>
---   buf_set_option("omnifunc", "v:lua.vim.lsp.omnifunc")
--- end
-
--- -- linters and formatters <https://www.lunarvim.org/docs/languages#lintingformatting>
-local formatters = require("lvim.lsp.null-ls.formatters")
-formatters.setup({
-	{ command = "stylua" },
-	{
-		command = "prettier",
-		extra_args = { "--print-width", "100" },
-		filetypes = { "typescript", "typescriptreact" },
-	},
-})
-local linters = require("lvim.lsp.null-ls.linters")
-linters.setup({
-	{ command = "flake8", filetypes = { "python" } },
-	{
-		command = "shellcheck",
-		args = { "--severity", "warning" },
-	},
-})
-
--- -- Additional Plugins <https://www.lunarvim.org/docs/plugins#user-plugins>
 lvim.plugins = {
-
-	{
-		"folke/trouble.nvim",
-		cmd = "TroubleToggle",
-	},
-	{ "catppuccin/nvim", name = "catppuccin", priority = 1000 },
-	{ "folke/tokyonight.nvim" },
-	{ "alker0/chezmoi.vim" },
-	{ "lunarvim/colorschemes" },
-	{ "kmonad/kmonad-vim" },
-	{
-		"ggandor/leap.nvim",
-		name = "leap",
-		config = function()
-			require("leap").add_default_mappings()
-		end,
-	},
-	{
-		"kevinhwang91/nvim-bqf",
-		event = { "BufRead", "BufNew" },
-		config = function()
-			require("bqf").setup({
-				auto_enable = true,
-				preview = {
-					win_height = 12,
-					win_vheight = 12,
-					delay_syntax = 80,
-					border_chars = { "┃", "┃", "━", "━", "┏", "┓", "┗", "┛", "█" },
-					func_map = {},
-					vsplit = "",
-					ptogglemode = "z,",
-					stoggleup = "",
+  { "catppuccin/nvim", name = "catppuccin",
+    opts = {
+      flavour = "mocha",
+			color_overrides = {
+				mocha = {
+					base = "#11111b",
+					mantle = "#11111b",
+					crust = "#11111b",
 				},
-				filter = {
-					fzf = {
-						action_for = { ["ctrl-s"] = "split" },
-						extra_opts = { "--bind", "ctrl-o:toggle-all", "--prompt", "> " },
-					},
-				},
-			})
-		end,
-	},
-	{
-		"nvim-neo-tree/neo-tree.nvim",
-		branch = "v2.x",
-		dependencies = {
-			"nvim-lua/plenary.nvim",
-			"nvim-tree/nvim-web-devicons",
-			"MunifTanjim/nui.nvim",
-		},
-		config = function()
-			require("neo-tree").setup({
-				close_if_last_window = true,
-				window = {
-					width = 35,
-					position = "right",
-					mappings = {
-						["z"] = "close_all_nodes",
-						["Z"] = "expand_all_nodes",
-					},
-				},
-				buffers = {
-					follow_current_file = true,
-				},
-				filesystem = {
-					follow_current_file = true,
-					filtered_items = {
-						hide_dotfiles = false,
-						hide_gitignored = false,
-						hide_by_name = {
-							"node_modules",
-						},
-						never_show = {
-							".DS_Store",
-							"thumbs.db",
-						},
-					},
-				},
-			})
-		end,
-	},
-	{
-		"kevinhwang91/rnvimr",
-		cmd = "RnvimrToggle",
-		config = function()
-			vim.g.rnvimr_draw_border = 1
-			vim.g.rnvimr_pick_enable = 1
-			vim.g.rnvimr_bw_enable = 1
-		end,
-	},
-	{
-		"ethanholz/nvim-lastplace",
-		event = "BufRead",
-		config = function()
-			require("nvim-lastplace").setup({
-				lastplace_ignore_buftype = { "quickfix", "nofile", "help" },
-				lastplace_ignore_filetype = {
-					"gitcommit",
-					"gitrebase",
-					"svn",
-					"hgcommit",
-				},
-				lastplace_open_folds = true,
-			})
-		end,
-	},
-	-- {
-	--   "andymass/vim-matchup",
-	--   event = "CursorMoved",
-	--   config = function()
-	--     vim.g.matchup_matchparen_offscreen = { method = "popup" }
-	--   end,
-	-- },
-	{
-		"nacro90/numb.nvim",
-		event = "BufRead",
-		config = function()
-			require("numb").setup({
-				show_numbers = true, -- Enable 'number' for the window while peeking
-				show_cursorline = true, -- Enable 'cursorline' for the window while peeking
-			})
-		end,
-	},
+			},
+    },
+  },
 }
 
--- Using neo-tree instead
-lvim.builtin.nvimtree.active = false -- NOTE: using neo-tree
-
--- enable treesitter integration
-lvim.builtin.treesitter.matchup.enable = true
-
--- -- Autocommands (`:help autocmd`) <https://neovim.io/doc/user/autocmd.html>
--- vim.api.nvim_create_autocmd("FileType", {
---   pattern = "zsh",
---   callback = function()
---     -- let treesitter use bash highlight for zsh files as well
---     require("nvim-treesitter.highlight").attach(0, "bash")
---   end,
--- })
+lvim.colorscheme = "catppuccin-mocha"
