7443613813c4b178e63e49ed97d441b5e578edd5
Eric Reeves <eric@alluvium.com>
2025-04-23 19:13:42 -0500

    Add .config/wezterm/LICENSE Add .config/wezterm/README.md Add .config/wezterm/backdrops/cloudy-quasar.png Add .config/wezterm/backdrops/nord-space.png Add .config/wezterm/colors/custom.lua Add .config/wezterm/config/appearance.lua Add .config/wezterm/config/bindings.lua Add .config/wezterm/config/domains.lua Add .config/wezterm/config/fonts.lua Add .config/wezterm/config/general.lua Add .config/wezterm/config/init.lua Add .config/wezterm/config/launch.lua Add .config/wezterm/.github/screenshots/screenshot-1.png Add .config/wezterm/.github/screenshots/screenshot-2.png Add .config/wezterm/.github/screenshots/wezterm.gif Add .config/wezterm/.github/workflows/lint.yml Add .config/wezterm/.gitignore Add .config/wezterm/.luacheckrc Add .config/wezterm/.luarc.json Add .config/wezterm/.stylua.toml Add .config/wezterm/events/left-status.lua Add .config/wezterm/events/new-tab-button.lua Add .config/wezterm/events/right-status.lua Add .config/wezterm/events/tab-title.lua Add .config/wezterm/utils/backdrops.lua Add .config/wezterm/utils/cells.lua Add .config/wezterm/utils/gpu-adapter.lua Add .config/wezterm/utils/math.lua Add .config/wezterm/utils/opts-validator.lua Add .config/wezterm/utils/platform.lua Update .config/wezterm/wezterm.lua

---
+++ dot_config/wezterm/config/fonts.lua
@@ -0,0 +1,23 @@
+local wezterm = require('wezterm')
+local platform = require('utils.platform')
+
+-- local font = 'Maple Mono SC NF'
+-- local font_family = 'JetBrainsMono Nerd Font'
+local font_family = 'FiraCode Nerd Font'
+local font_size = platform.is_mac and 12 or 11
+
+return {
+   font = wezterm.font_with_fallback({
+    { family = font_family, weight = 'Medium' },
+    { family = 'Symbols Nerd Font' },
+    { family = 'Noto Color Emoji' },
+    { family = 'NotoMono Nerd Font' },
+   }),
+   font_size = font_size,
+
+   --ref: https://wezfurlong.org/wezterm/config/lua/config/freetype_pcf_long_family_names.html#why-doesnt-wezterm-use-the-distro-freetype-or-match-its-configuration
+   freetype_load_target = 'Normal', ---@type 'Normal'|'Light'|'Mono'|'HorizontalLcd'
+   freetype_render_target = 'Normal', ---@type 'Normal'|'Light'|'Mono'|'HorizontalLcd'
+}
+
+  -- font = wezterm.font_with_fallback({"JetBrains Mono", "Symbols Nerd Font"}),
--- dot_config/wezterm/wezterm.lua
+++ dot_config/wezterm/wezterm.lua
@@ -1,81 +1,26 @@
-------------------------------------------------------
---               Wezterm configuration
-------------------------------------------------------
+local Config = require('config')
+
+require('utils.backdrops')
+   -- :set_focus('#000000')
+   -- :set_images_dir(require('wezterm').home_dir .. '/Pictures/Wallpapers/')
+   :set_images()
+   :random()
+
+require('events.left-status').setup()
+require('events.right-status').setup({ date_format = '%a %H:%M:%S' })
+require('events.tab-title').setup({ hide_active_tab_unseen = false, unseen_icon = 'circle' })
+require('events.new-tab-button').setup()
+
+local config = Config:init()
+:append(require('config.appearance'))
+:append(require('config.bindings'))
+:append(require('config.domains'))
+:append(require('config.fonts'))
+:append(require('config.general'))
+:append(require('config.launch')).options
 
 local wezterm = require("wezterm")
-local config = {}
-
-if wezterm.config_builder then
-	config = wezterm.config_builder()
-end
-
-local windows = require("windows") -- file located at ~/.wezterm/windows.lua
-local linux = require("linux") -- file located at ~/.wezterm/linux.lua
-local keymaps = require("keymaps")
-local terminal = require("terminal")
--- local bar = wezterm.plugin.require("https://github.com/adriankarlen/bar.wezterm")
-local tabline = wezterm.plugin.require("https://github.com/michaelbrusegard/tabline.wez")
--- For windows host custom configuration
-if wezterm.target_triple == "x86_64-pc-windows-msvc" then
-	windows.options(config)
-end
-
--- For linux host custom configuration
-if wezterm.target_triple == "x86_64-unknown-linux-gnu" then
-	linux.options(config)
-end
-
-keymaps.options(config)
-terminal.options(config)
-
--- bar.apply_to_config(config)
--- Add spotify to bar
--- bar.apply_to_config(config, {
--- 	modules = {
--- 		spotify = {
--- 			enabled = true,
--- 		},
--- 	},
--- })
-
-tabline.setup({
-	options = {
-		icons_enabled = true,
-		theme = "Catppuccin Mocha",
-		tabs_enabled = true,
-		theme_overrides = {},
-		section_separators = {
-			left = wezterm.nerdfonts.pl_left_hard_divider,
-			right = wezterm.nerdfonts.pl_right_hard_divider,
-		},
-		component_separators = {
-			left = wezterm.nerdfonts.pl_left_soft_divider,
-			right = wezterm.nerdfonts.pl_right_soft_divider,
-		},
-		tab_separators = {
-			left = wezterm.nerdfonts.pl_left_hard_divider,
-			right = wezterm.nerdfonts.pl_right_hard_divider,
-		},
-	},
-	sections = {
-		tabline_a = { "mode" },
-		tabline_b = { "workspace" },
-		tabline_c = { " " },
-		tab_active = {
-			"index",
-			{ "parent", padding = 0 },
-			"/",
-			{ "cwd", padding = { left = 0, right = 1 } },
-			{ "zoomed", padding = 0 },
-		},
-		tab_inactive = { "index", { "process", padding = { left = 0, right = 1 } } },
-		tabline_x = { "ram", "cpu" },
-		tabline_y = { "datetime", "battery" },
-		tabline_z = { "domain" },
-	},
-	extensions = {},
-})
-
-tabline.apply_to_config(config)
+local modal = wezterm.plugin.require("https://github.com/MLFlexer/modal.wezterm")
+modal.apply_to_config(config)
 
 return config
