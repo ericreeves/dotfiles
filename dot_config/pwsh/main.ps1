3e854b3cdb51ef41e28d0ab13515534bfe26ca40
Eric Reeves <eric@alluvium.com>
2025-06-05 18:47:40 -0500

    Update .config/pwsh/main.ps1

--- dot_config/pwsh/main.ps1
+++ dot_config/pwsh/main.ps1
@@ -1,10 +1,12 @@
+if (Test-Path $Env:USERPROFILE"\.config\pwsh\env-secret.ps1") { . $Env:USERPROFILE"\.config\pwsh\env-secret.ps1 }
+
 . $Env:USERPROFILE"\.config\pwsh\modules.ps1"
-. $Env:USERPROFILE"\.config\pwsh\environment.ps1"
+. $Env:USERPROFILE"\.config\pwsh\env.ps1"
 . $Env:USERPROFILE"\.config\pwsh\alias.ps1"
 . $Env:USERPROFILE"\.config\pwsh\utility.ps1"
 
+
 if (Get-Command "starship" -ErrorAction SilentlyContinue) {
   $ENV:STARSHIP_CONFIG = "$( $HOME )/.config/starship.toml"
-  # $ENV:STARSHIP_DISTRO = "SKY"
   Invoke-Expression (&starship init powershell)
 }
