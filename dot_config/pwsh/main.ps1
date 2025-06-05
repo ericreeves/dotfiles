55f3e504c5fed9b8aa311240a70600a84d57aa9b
Eric Reeves <eric@alluvium.com>
2025-06-05 18:58:51 -0500

    Update .config/pwsh/main.ps1

--- dot_config/pwsh/main.ps1
+++ dot_config/pwsh/main.ps1
@@ -3,7 +3,6 @@ if (Test-Path $Env:USERPROFILE"\.config\pwsh\env-secret.ps1") { . $Env:USERPROFI
 . $Env:USERPROFILE"\.config\pwsh\modules.ps1"
 . $Env:USERPROFILE"\.config\pwsh\env.ps1"
 . $Env:USERPROFILE"\.config\pwsh\alias.ps1"
-. $Env:USERPROFILE"\.config\pwsh\utility.ps1"
 
 
 if (Get-Command "starship" -ErrorAction SilentlyContinue) {
