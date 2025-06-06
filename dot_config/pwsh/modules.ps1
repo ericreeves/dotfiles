b43c0b58d47914b5e571cb7e280beeafc2714834
Eric Reeves <eric@alluvium.com>
2025-06-05 19:23:36 -0500

    Update .config/pwsh/modules.ps1

--- dot_config/pwsh/modules.ps1
+++ dot_config/pwsh/modules.ps1
@@ -1,29 +1,31 @@
-Import-Module gsudoModule
-# Import-Module -Name HackF5.ProfileAlias -Force -Global -ErrorAction SilentlyContinue
+$modules = @(
+    'Terminal-Icons'
+    'PSReadLine'
+    'gsudoModule'
+)
 
-# if ($host.Name -eq 'ConsoleHost')
-# {
-  Import-Module PSReadLine
-  Import-Module -Name Terminal-Icons
-  # Invoke-Expression (& {
-  #     $hook = if ($PSVersionTable.PSVersion.Major -lt 6) { 'prompt' } else { 'pwd' }
-  #     })
-  #     (zoxide init --hook $hook powershell | Out-String)
-  #
-  Set-PSReadlineKeyHandler -Key Tab -Function Complete
-  Set-PSReadlineOption -EditMode vi
-  Set-PSReadlineOption -PredictionViewStyle ListView
-  Set-PSReadlineKeyHandler -Key UpArrow -Function HistorySearchBackward
-  Set-PSReadlineKeyHandler -Key DownArrow -Function HistorySearchForward
-  Set-PSReadLineOption -PredictionSource History
-  # # https://gist.github.com/wilsnat/b51d2211c94d39536d6e84b59cb659bf
-  Set-PSReadLineOption -HistoryNoDuplicates
-  Set-PSReadLineOption -HistorySearchCursorMovesToEnd
-  Set-PSReadLineOption -HistorySaveStyle SaveIncrementally
-  Set-PSReadLineOption -MaximumHistoryCount 2000
-  Set-PSReadLineKeyHandler -Key UpArrow -Function HistorySearchBackward
-  Set-PSReadLineKeyHandler -Key DownArrow -Function HistorySearchForward
-  Set-PSReadLineKeyHandler -Chord 'Shift+Tab' -Function Complete
-  Set-PSReadLineKeyHandler -Key Tab -Function MenuComplete
-# }
-  Invoke-Expression (& { (zoxide init powershell | Out-String) })
+foreach ($module in $modules) {
+    if (-not (Get-Module -ListAvailable -Name $module)) {
+        Install-Module -Name $module -Scope CurrentUser -Force -SkipPublisherCheck
+    }
+    Import-Module -Name $module
+}
+
+Invoke-Expression (& { (sfsu hook --disable list | Out-String) }) # Faster scoop searching
+Invoke-Expression (& { (zoxide init powershell | Out-String) })
+
+Set-PSReadlineKeyHandler -Key Tab -Function Complete
+Set-PSReadlineOption -EditMode vi
+Set-PSReadlineOption -PredictionViewStyle ListView
+Set-PSReadlineKeyHandler -Key UpArrow -Function HistorySearchBackward
+Set-PSReadlineKeyHandler -Key DownArrow -Function HistorySearchForward
+Set-PSReadLineOption -PredictionSource History
+# # https://gist.github.com/wilsnat/b51d2211c94d39536d6e84b59cb659bf
+Set-PSReadLineOption -HistoryNoDuplicates
+Set-PSReadLineOption -HistorySearchCursorMovesToEnd
+Set-PSReadLineOption -HistorySaveStyle SaveIncrementally
+Set-PSReadLineOption -MaximumHistoryCount 2000
+Set-PSReadLineKeyHandler -Key UpArrow -Function HistorySearchBackward
+Set-PSReadLineKeyHandler -Key DownArrow -Function HistorySearchForward
+Set-PSReadLineKeyHandler -Chord 'Shift+Tab' -Function Complete
+Set-PSReadLineKeyHandler -Key Tab -Function MenuComplete
