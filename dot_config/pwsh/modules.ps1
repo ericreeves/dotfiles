0f495626b743609c45fde3c658cc264f7d1ef38f
Eric Reeves <eric@alluvium.com>
2025-06-05 19:43:26 -0500

    Update .config/pwsh/modules.ps1

--- dot_config/pwsh/modules.ps1
+++ dot_config/pwsh/modules.ps1
@@ -1,6 +1,7 @@
 $modules = @(
     'Terminal-Icons'
     'PSReadLine'
+    'PSFzf'
     'gsudoModule'
 )
 
@@ -11,21 +12,3 @@ foreach ($module in $modules) {
     Import-Module -Name $module
 }
 
-Invoke-Expression (& { (sfsu hook --disable list | Out-String) }) # Faster scoop searching
-Invoke-Expression (& { (zoxide init powershell | Out-String) })
-
-Set-PSReadlineKeyHandler -Key Tab -Function Complete
-Set-PSReadlineOption -EditMode vi
-Set-PSReadlineOption -PredictionViewStyle ListView
-Set-PSReadlineKeyHandler -Key UpArrow -Function HistorySearchBackward
-Set-PSReadlineKeyHandler -Key DownArrow -Function HistorySearchForward
-Set-PSReadLineOption -PredictionSource History
-# # https://gist.github.com/wilsnat/b51d2211c94d39536d6e84b59cb659bf
-Set-PSReadLineOption -HistoryNoDuplicates
-Set-PSReadLineOption -HistorySearchCursorMovesToEnd
-Set-PSReadLineOption -HistorySaveStyle SaveIncrementally
-Set-PSReadLineOption -MaximumHistoryCount 2000
-Set-PSReadLineKeyHandler -Key UpArrow -Function HistorySearchBackward
-Set-PSReadLineKeyHandler -Key DownArrow -Function HistorySearchForward
-Set-PSReadLineKeyHandler -Chord 'Shift+Tab' -Function Complete
-Set-PSReadLineKeyHandler -Key Tab -Function MenuComplete
