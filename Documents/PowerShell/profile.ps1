162a3fb13182e7795e53a7f99f8e46f7025efb5d
Eric Reeves <eric@alluvium.com>
2025-07-11 14:26:28 -0500

    Update Documents/PowerShell/profile.ps1

--- Documents/PowerShell/profile.ps1
+++ Documents/PowerShell/profile.ps1
@@ -1,1 +1,9 @@
 . $Env:USERPROFILE"\.config\pwsh\main.ps1"
+
+#region conda initialize
+# !! Contents within this block are managed by 'conda init' !!
+If (Test-Path "C:\Users\eric\scoop\apps\miniconda3\current\Scripts\conda.exe") {
+    (& "C:\Users\eric\scoop\apps\miniconda3\current\Scripts\conda.exe" "shell.powershell" "hook") | Out-String | ?{$_} | Invoke-Expression
+}
+#endregion
+
