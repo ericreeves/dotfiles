3c96ba43f23083859670943b4bf3e3cd24b86029
ericreeves <eric@coldfire.cc>
2026-03-05 12:41:53 -0600

    Add .local/bin/g9.sh

---
+++ dot_local/bin/g9.sh
@@ -0,0 +1,5 @@
+# goto tv home screen
+smartthings devices:commands <device-id >'samsungvd.remoteControl:send("HOME")'
+
+# switch to display port
+smartthings devices:commands <device-id >'samsungvd.mediaInputSource:setInputSource("Display Port")'
