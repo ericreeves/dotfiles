36ff2cb6afa64aa41fbcdb2022fba54694525861
ericreeves <eric@coldfire.cc>
2026-04-02 08:59:24 -0500

    Add .config/colorscheme.sh

---
+++ dot_config/executable_colorscheme.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# Central colorscheme — single source of truth
+# All tools (sketchybar, borders, etc.) source this file
+# iOS dark theme inspired by komorebi-bar
+
+export COLOR_BG="1c1c1e"
+export COLOR_BG_SECONDARY="2c2c2e"
+export COLOR_BG_TERTIARY="3a3a3c"
+export COLOR_FG="ebebf5"
+export COLOR_FG_SECONDARY="f2f2f7"
+export COLOR_SUBTLE="636366"
+export COLOR_INACTIVE="48484a"
+export COLOR_ACCENT="e0e0e4"
+export COLOR_RED="ff453a"
+export COLOR_ORANGE="ff9f0a"
+export COLOR_YELLOW="ffd60a"
+export COLOR_GREEN="30d158"
+export COLOR_BLUE="64d2ff"
+export COLOR_PURPLE="bf5af2"
+export COLOR_PINK="ff375f"
+export COLOR_LAVENDER="b4befe"
