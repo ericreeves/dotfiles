2f1eeadfb15a0b5d9fd690880fd045d6a46c90e3
ericreeves <eric@coldfire.cc>
2026-04-02 08:58:56 -0500

    Add .config/aerospace/sketchybar/plugins/front_app.sh

---
+++ dot_config/aerospace/sketchybar/plugins/executable_front_app.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+source "$CONFIG_DIR/icon_map.sh"
+
+if [ "$SENDER" = "front_app_switched" ]; then
+  __icon_map "$INFO"
+  sketchybar --set "$NAME" label="$INFO" icon="$icon_result"
+fi
